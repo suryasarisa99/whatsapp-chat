fcf2fea87709af7e932d640f3a8eda0057c7a7eb
surya <suryasarisa99@gmail.com>
2023-10-22 16:09:04 +0530

    updated chat-item

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { chat2 } from "../data2";
 import BottomBar from "../components/BottomBar";
 import HomeTopBar from "../components/HomeTopBar";
 import { BiSolidMessageAdd } from "react-icons/bi";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 import Dp from "../components/blank-profile-picture-973460_1280.png";
 export default function Home() {
   const { setFileContent, fileName, setFileName } = useContext(DataContext);
@@ -39,32 +40,22 @@ export default function Home() {
       />
 
       <div className="home-chats">
-        <div
-          className="chat-item"
+        <ChatSection
           onClick={() => {
             setFileContent(chat2);
             navigate("/chat");
           }}
-        >
-          <img src={Dp} />
-          <div className="people">
-            <div className="sender">Vijay Btech</div>
-            <div className="reciver">Tarun Kumar</div>
-          </div>
-        </div>
-        <div
-          className="chat-item"
+          s="Vijaya Btech"
+          r="Tarun Kumar"
+        />
+        <ChatSection
           onClick={() => {
-            setFileContent(chat2);
+            setFileContent(chat1);
             navigate("/chat");
           }}
-        >
-          <img src={Dp} />
-          <div className="people">
-            <div className="sender">Barbie Girl</div>
-            <div className="reciver">Anil Kumar</div>
-          </div>
-        </div>
+          s="Anil Kumar"
+          r="Barbie Girl"
+        />
       </div>
 
       <div
@@ -81,29 +72,37 @@ export default function Home() {
   );
 }
 
-{
-  /* <button
-className=""
-onClick={(e) => {
-  inputRef.current.click();
-}}
->
-Select File
-</button>
-<button
-onClick={() => {
-  setFileContent(chat1);
-  navigate("/chat");
-}}
->
-Sample 1
-</button>
-<button
-onClick={() => {
-  setFileContent(chat2);
-  navigate("/chat");
-}}
->
-Sample 2
-</button> */
+function ChatSection({ onClick, s, r }) {
+  return (
+    <div className="chat-item" onClick={onClick}>
+      <img src={Dp} />
+      <div className="people">
+        <div className="p1">
+          <FaLongArrowAltLeft className="icon" />
+          <div className="sender">{s}</div>
+        </div>
+        <div className="p2">
+          <div className="reciver">{r}</div>
+          <FaLongArrowAltRight className="icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
+function ChatSection2({ onClick, s, r }) {
+  return (
+    <div className="chat-item" onClick={onClick}>
+      <img src={Dp} />
+      <div className="people">
+        <div className="p1">
+          <FaLongArrowAltLeft className="icon" />
+          <div className="sender">{s}</div>
+        </div>
+        <div className="p2">
+          <div className="reciver">{r}</div>
+          <FaLongArrowAltRight className="icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
