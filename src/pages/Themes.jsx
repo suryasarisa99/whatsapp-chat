e1c8c1268e9d9585cbafb0849cc177b998e9a532
surya <suryasarisa99@gmail.com>
2023-10-22 20:52:28 +0530

    added themes

--- src/pages/Themes.jsx
+++ src/pages/Themes.jsx
@@ -5,6 +5,17 @@ import { useNavigate } from "react-router-dom";
 export default function Themes() {
   const navigate = useNavigate();
   const themes = ["default", "gray"];
+
+  const handleChangeBackgroundColor = () => {
+    // Update the manifest file
+    const manifest = {
+      ...window.navigator.serviceWorker.manifest,
+      background_color: "#181c21",
+      theme_color: "#181c21",
+    };
+
+    window.navigator.serviceWorker.register(manifest);
+  };
   return (
     <div className="themes-page">
       <div className="themes">
@@ -16,6 +27,7 @@ export default function Themes() {
               document.documentElement.className = "";
               document.documentElement.classList.add(themes[ind]);
               localStorage.setItem("theme", themes[ind]);
+              handleChangeBackgroundColor();
               navigate(-1);
             }}
           ></div>
